30dfeeb869cb2159ca012a1519c9782875cb43a4
Camara Laby Damaro <ldamaro98@gmail.com>
2019-02-20 23:02:08 +0100

    Création des classes: ApplicationComponent,Router et Route et le ficher route.xml

---
+++ CamaraTek/ApplicationComponent.php
@@ -0,0 +1,21 @@
+<?php
+	
+	namespace CamaraTek;
+
+	/**
+	 * 
+	 */
+	abstract class ApplicationComponent
+	{
+		protected $app;
+		
+		public function __construct(Application $app)
+		{
+			$this->app = $app;
+		}
+
+		public function app()
+		{
+			return $this->app;
+		}
+	}
